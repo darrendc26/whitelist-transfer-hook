e5dc0b0be723af681de7707b1a95eca7aefdd69d
Darren da Costa <darren26feb@gmail.com>
2026-02-04 00:45:09 +0530

    Refactor extra_account_metas

--- programs/whitelist-transfer-hook/src/instructions/init_extra_account_meta.rs
+++ programs/whitelist-transfer-hook/src/instructions/init_extra_account_meta.rs
@@ -1,8 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token_interface::Mint;
 use spl_tlv_account_resolution::{
-    account::ExtraAccountMeta, 
-    state::ExtraAccountMetaList
+    account::ExtraAccountMeta, seeds::Seed, state::ExtraAccountMetaList,
 };
 
 use crate::ID;
@@ -30,15 +29,21 @@ pub struct InitializeExtraAccountMetaList<'info> {
 impl<'info> InitializeExtraAccountMetaList<'info> {
     pub fn extra_account_metas() -> Result<Vec<ExtraAccountMeta>> {
         // Derive the whitelist PDA using our program ID
-        let (whitelist_pda, _bump) = Pubkey::find_program_address(
-            &[b"whitelist"],
-            &ID
-        );
-        
-        Ok(
-            vec![
-                ExtraAccountMeta::new_with_pubkey(&whitelist_pda.to_bytes().into(), false, false).unwrap(),
-            ]
+        // let (whitelist_pda, _bump) = Pubkey::find_program_address(
+        //     &[b"whitelist"],
+        //     &ID
+        // );
+
+        Ok(vec![ExtraAccountMeta::new_with_seeds(
+            &[
+                Seed::Literal {
+                    bytes: b"whitelist".to_vec(),
+                },
+                Seed::AccountKey { index: 3 },
+            ],
+            false,
+            false,
         )
+        .unwrap()])
     }
 }
